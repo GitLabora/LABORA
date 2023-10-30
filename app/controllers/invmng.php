e4f97158377aa2e400aee31b08ffc569dbf521df
PraveenaRupasinghe <sapvrupasinghe@gmail.com>
2023-10-30 20:27:11 +0530

    invmng controller 1

---
+++ app/controllers/invmng.php
@@ -0,0 +1,138 @@
+<?php
+    class invmng extends Controller{
+        public function __construct(){
+
+        }
+
+        public function order(){
+            if(!isset($_SESSION['userid'])){
+                header("location: http://localhost/labora/user/logout");
+            }
+            // }else{
+            //     if((time()-$_SESSION['last_login_timestamp'])>600){
+            //         header("location: http://localhost/labora/user/logout");
+            //     }else{
+            //         $_SESSION['last_login_timestamp'] = time();
+            //     }
+            // }
+
+            $data = [];
+            $this->view("invmng/order" , $data);
+        }
+
+        public function product(){
+            if(!isset($_SESSION['userid'])){
+                header("location: http://localhost/labora/user/logout");
+            }
+            // }else{
+            //     if((time()-$_SESSION['last_login_timestamp'])>600){
+            //         header("location: http://localhost/labora/user/logout");
+            //     }else{
+            //         $_SESSION['last_login_timestamp'] = time();
+            //     }
+            // }
+
+            $data = [];
+            $this->view("invmng/product" , $data);
+        }
+
+        public function dashboard(){
+            if(!isset($_SESSION['userid'])){
+                header("location: http://localhost/labora/user/logout");
+            }
+            // }else{
+            //     if((time()-$_SESSION['last_login_timestamp'])>600){
+            //         header("location: http://localhost/labora/user/logout");
+            //     }else{
+            //         $_SESSION['last_login_timestamp'] = time();
+            //     }
+            // }
+
+            $data = [];
+            $this->view("invmng/dashboard" , $data);
+        }
+
+        public function supplier(){
+            if(!isset($_SESSION['userid'])){
+                header("location: http://localhost/labora/user/logout");
+            }
+            // }else{
+            //     if((time()-$_SESSION['last_login_timestamp'])>600){
+            //         header("location: http://localhost/labora/user/logout");
+            //     }else{
+            //         $_SESSION['last_login_timestamp'] = time();
+            //     }
+            // }
+
+            $data = [];
+            $this->view("invmng/supplier" , $data);
+        }
+
+        public function expiredChemicals(){
+            if(!isset($_SESSION['userid'])){
+                header("location: http://localhost/labora/user/logout");
+            }
+            // }else{
+            //     if((time()-$_SESSION['last_login_timestamp'])>600){
+            //         header("location: http://localhost/labora/user/logout");
+            //     }else{
+            //         $_SESSION['last_login_timestamp'] = time();
+            //     }
+            // }
+
+            $data = [];
+            $this->view("invmng/expiredChemicals" , $data);
+        }
+
+        public function reorder(){
+            if(!isset($_SESSION['userid'])){
+                header("location: http://localhost/labora/user/logout");
+            }
+            // }else{
+            //     if((time()-$_SESSION['last_login_timestamp'])>600){
+            //         header("location: http://localhost/labora/user/logout");
+            //     }else{
+            //         $_SESSION['last_login_timestamp'] = time();
+            //     }
+            // }
+
+            $data = [];
+            $this->view("invmng/reorder" , $data);
+        }
+
+        public function invoices(){
+            if(!isset($_SESSION['userid'])){
+                header("location: http://localhost/labora/user/logout");
+            }
+            // }else{
+            //     if((time()-$_SESSION['last_login_timestamp'])>600){
+            //         header("location: http://localhost/labora/user/logout");
+            //     }else{
+            //         $_SESSION['last_login_timestamp'] = time();
+            //     }
+            // }
+
+            $data = [];
+            $this->view("invmng/invoices" , $data);
+        }
+
+        public function quotations(){
+            if(!isset($_SESSION['userid'])){
+                header("location: http://localhost/labora/user/logout");
+            }
+            // }else{
+            //     if((time()-$_SESSION['last_login_timestamp'])>600){
+            //         header("location: http://localhost/labora/user/logout");
+            //     }else{
+            //         $_SESSION['last_login_timestamp'] = time();
+            //     }
+            // }
+
+            $data = [];
+            $this->view("invmng/quotations" , $data);
+        }
+
+
+        
+    }
+?>

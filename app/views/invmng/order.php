ce004494ca55d312ddc454bc9c5431cde225e79f
PraveenaRupasinghe <sapvrupasinghe@gmail.com>
2023-10-31 15:45:25 +0530

    view button php

--- app/views/invmng/order.php
+++ app/views/invmng/order.php
@@ -61,7 +61,7 @@
         <td>Pending</td>
         <td>Pending</td>
         
-        <td><button type="submit" class="submit button">View</button></a>
+        <td><a href="#" ><button>View</button></a>
                 </td>
        
        
@@ -77,7 +77,7 @@
         <td>Confirmed</td>
         <td>Approved</td>
         
-        <td><button type="submit" class="submit button">View</button></a>
+        <td><a href="#" ><button>View</button></a>
                 </td>
        
        

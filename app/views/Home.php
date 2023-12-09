25a1267f8d8cd024774a18b834ed33374d0bf57b
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2023-12-09 11:31:28 +0530

    Home page and login page update

--- app/views/Home.php
+++ app/views/Home.php
@@ -5,106 +5,122 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="<?php echo APPROOT.'\public\css\styles.css';?>">
-    <script src="<?php echo APPROOT.'\public\js\home.js';?>"></script>
+
 </head>
-<body>
+<body onload="service()">
     <div class="container">
-        <div class="header">
         <nav>
             <img src="<?php echo APPROOT.'/public/';?>img/Health_Care__2_-removebg-preview.png" alt="" class="logo">
             <ul>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#services">Services</a></li>
                 <li><a href="#aboutUs">About Us</a></li>
-                <li><a href="#contact">Contact Us</a></li>
+                <li><a href="#contact-section">Contact Us</a></li>
+                <li><a href="<?php echo URLROOT?>user/login" class="signin-button button">Sign In</a></li>
             </ul>
-        <nav>
-            <a href="<?php echo URLROOT?>user/login" class="signin-button button">Sign In</a>
-        </div>
+        </nav>
         <div class="hero">
-        <h1>Welcome to <span class="SAHANYA Labs">SAHANYA Labs</span></h1>
-        <h4>Setting the Standard in Healthcare Excellence</h4>
-        <p>Welcome to SAHANYA Labs, your trusted partner in precision medical testing and healthcare excellence. With a legacy of accuracy and a commitment to your well-being, we provide reliable results. Explore advanced diagnostics and personalized care for your health.</p><br>
-        <a href="<?php echo URLROOT?>user/register" class="signup-button button">Get Started</a>
+            <div>
+                <h1>Welcome to <span class="SAHANYA Labs">SAHANYA Labs</span></h1>
+                <h4>Setting the Standard in Healthcare Excellence</h4>
+                <p>Welcome to SAHANYA Labs, your trusted partner in precision medical testing and healthcare excellence. With a legacy of accuracy and a commitment to your well-being, we provide reliable results. Explore advanced diagnostics and personalized care for your health.</p><br>
+                <a href="<?php echo URLROOT?>user/register" class="signup-button button">Get Started</a>
+            </div>
+            <div>
+            <img src="<?php echo APPROOT.'/public/';?>img/DNA.png" alt="" class="logo">
+            </div>
+            
         </div>
-        
     </div>
     <div class="services" id="services">
-        <h1>Our Comprehensive Services</h1>
-        <div class="horizontal-line"></div>
-        <h3>Advanced Medical Testing Tailored to Your Needs</h3>
-        <p>At SAHANYA Labs, we offer a wide range of medical tests and diagnostics to cater to your healthcare needs. Our dedicated team of professionals ensures accurate and timely results. Below are some of the key tests we provide, along with important pre-preparation information.</p>
-        <h4>Available Medical Test</h4>
-        <div class="test-list">
+        <div class="service-title">
             <div>
-                <ul>
-                <li><a onclick="CBC();" id="first">Complete Blood Count (CBC)</a></li>
-                <li><a onclick="Lipid();">Lipid Profile</a></li>
-                <li><a onclick="Glucose();">Blood Glucose Test</a></li>
-                <li><a onclick="Urinalysis();">Urinalysis</a></li>
-                <li><a onclick="Density();">Bone Density Scan (DXA)</a></li>
-                <li><a onclick="XRay();">X-Ray (Radiography)</a></li>
-                <li><a onclick="ECG();">Electrocardiogram (ECG or EKG)</a></li>
-                <li><a onclick="BPM();">Blood Pressure Measurement</a></li>
-                <li><a onclick="MRI();">MRI (Magnetic Resonance Imaging)</a></li>
-                <li><a onclick="Pap();">Pap Smear</a></li>
-            </ul>
+                <img src="<?php echo APPROOT.'/public/';?>img/Services.png" alt="" class="logo">
             </div>
-            <div class="vertical-line"></div>
-            <div class="discription">
-                <p id="dis">    
-                    
-                </p>
+            <div class="title-text">
+                <h3>Advanced Medical Testing Tailored to Your Needs</h3>
+                <h1>Our Comprehensive Services</h1>
+                <p>At SAHANYA Labs, we offer a wide range of medical tests and diagnostics to cater to your healthcare needs. Our dedicated team of professionals ensures accurate and timely results. Below are some of the key tests we provide, along with important pre-preparation information.</p>
+            </div>
+        </div>
+        <div class="test-type">
+            <div class="test-search">
+                <input id="search1" type="search" placeholder="Search here" data-search>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <div class="test-list">
+                    <ul id="service-list">
+                    </ul>
+                </div>
+                <div>
+                    <img src="<?php echo APPROOT.'/public/';?>img/Science.png" alt="" class="logo">
+                </div>
+            </div>
+            <div class="test-description">
+                <h4><i class="fa-solid fa-file-prescription"></i></i> Description</h4>
+                <p id="dis" class="dis"></p>
+                <h4><i class="fa-solid fa-user-doctor"></i> Preparation</h4>
+                <p id="pre" class="pre"></p>
             </div>
         </div>
     </div>
     <div class="aboutus" id="aboutUs">
-        <div class="bimg"><br>
-        <h1>About <span>SAHANYA Labs</span></h1>
-        <div class="horizontal-line"></div>
-        <p>SAHANYA Labs holds a prominent position within the medical SAHANYA Labstory landscape, earning recognition for its unwavering dedication to the utmost precision, unparalleled reliability, and a relentless pursuit of excellence in diagnostics. With a history dating back to our establishment in 2010, we have remained steadfast in our mission to consistently establish and maintain the highest standards in quality healthcare services. Over the years, our commitment to excellence has evolved into a legacy, setting a benchmark that inspires trust among our patients and partners in the healthcare community</p>
+        <div class="aboutus-title"><br>
+            <h1>About <span>SAHANYA Labs</span></h1>
+        </div>
+        <div class="description">
+            <h2>
+            Sahanya Labs is a leading medical laboratory committed to providing precise diagnostics through advanced technology and expertise. Our focus on quality and innovation empowers healthcare providers and patients.
+            </h2>
+        </div>
+        <div class="short-description">
+            <p>SAHANYA Labs holds a prominent position within the medical SAHANYA Labstory landscape, earning recognition for its unwavering dedication to the utmost precision, unparalleled reliability, and a relentless pursuit of excellence in diagnostics. With a history dating back to our establishment in 2010, we have remained steadfast in our mission to consistently establish and maintain the highest standards in quality healthcare services. Over the years, our commitment to excellence has evolved into a legacy, setting a benchmark that inspires trust among our patients and partners in the healthcare community</p>
+        </div>
+        <div class="imgs">
+            <img src="<?php echo APPROOT.'/public/img/award1.png';?>" alt="image not found">
+            <img src="<?php echo APPROOT.'/public/img/award2.png';?>" alt="image not found">
+            <img src="<?php echo APPROOT.'/public/img/award3.png';?>" alt="image not found">
         </div>
         <div class="award">
-            <h1>Our Awards and Recognitions</h1>
-            <div class="horizontal-line"></div>
-            <div class="awards">
-                <div class="award1 aw">
-                    <img src="<?php echo APPROOT.'/public/img/award1.png';?>" alt="image not found">
-                    <h3>Clinical Excellence Award of the year</h3>
-                    <p>Recognizing outstanding accuracy and quality in diagnostic testing.</p>
-                </div>
-                <div class="award2 aw">
-                    <img src="<?php echo APPROOT.'/public/img/award2.png';?>" alt="image not found">
-                    <h3>Innovation in SAHANYA Labstory Technology Award</h3>
-                    <p>Celebrating SAHANYA Labstories driving advancements in diagnostic.</p>
-                </div>
-                <div class="award3 aw">
-                    <img src="<?php echo APPROOT.'/public/img/award3.png';?>" alt="image not found">
-                    <h3>Patient-Centric SAHANYA Labstory of the Year</h3>
-                    <p>Honoring labs prioritizing patient care and satisfaction.</p>
-                </div>
+            <div class="award1 aw">
+                <h3>Clinical Excellence Award of the year</h3>
+                <p>Acknowledging exceptional precision and excellence in the field of diagnostic testing, we recognize and commend the noteworthy standards achieved in terms of accuracy and quality within this vital aspect of healthcare services. </p>
+            </div>
+            <div class="award2 aw">
+                <h3>SAHANYA Labstory Technology Award</h3>
+                <p>In joyous commemoration, we take a moment to celebrate the impactful narrative of SAHANYA Labstories, a narrative that unfolds as a compelling saga of driving substantial advancements in the realm of diagnostics.</p>
+            </div>
+            <div class="award3 aw">
+                <h3>SAHANYA Labstory of the Year</h3>
+                <p>In an earnest tribute, we extend our heartfelt appreciation to laboratories that place an unwavering emphasis on the paramount values of patient care and satisfaction.</p>
             </div>
         </div>
     </div>
+
+    <div class="contact-section" id="contact-section"><h1>Contact <span>SAHANYA Labs</span></h1></div>
     <div class="contact" id="contact">
-        <div class="contact-section"><h1><br>Contact <span>SAHANYA Labs</h1></span><br><div class="horizontal-line"></div></div>
+        <div class="background">
+            <h2>Get In Touch</h2>
+            <p>We’re here to help and answer any questions you might have. We will answer your inquiries in a maximum of 48 hours.</p>
+            <p><i class="fa-solid fa-location-dot"></i> 123, Galle Road,Colombo 00500.</p>
+            <p><i class="fa-solid fa-envelope"></i> Sahanyalabs@gmail.com</p>
+            <p><i class="fa-brands fa-facebook"></i> SahanyaLabs</p>
+        </div> 
+
         <div class="contact-form">
-        <form action="submit.php" method="post">
-            <fieldset class="getintouch">
-                <legend><h2>Get In Touch</h2></legend>
-                <input type="text" id="name" name="name" placeholder="Name" required>
-
-                <input type="email" id="email" name="email" placeholder="Email" required><br>
-
-                <input type="text" id="subject" name="subject" placeholder="Subject" required><br>
-
-                <textarea id="message" name="message" rows="4" placeholder="Message" required></textarea><br>
-
-                <input type="submit" value="Submit" class="submit button">
-            </fieldset>
-            
-        </form>
-        </div>
+                <form action="submit.php" method="post">
+                    <label for="name">Name*</label><br>
+                    <input type="text" id="name" pattern="[A-Za-z. ]+" required><br>
+                    <label for="email">Email*</label><br>
+                    <input type="email" id="email" name="email"  required><br>
+                    <label for="tel">Phone Number*</label><br>
+                    <input type="text" id="tel" name="tel"  minlength="10" required><br>
+                    <label for="subject">Subject*</label><br>
+                    <input type="text" id="subject" name="subject" required><br>
+                    <label for="message">Message*</label><br>
+                    <input id="message" name="message" rows="1" placeholder="Write your message here..." required></input><br>
+                    <input type="submit" value="Submit" class="submit button">
+                </form>
+        </div>   
     </div>
     <footer class="footer">
     <div class="footer__addr">
@@ -161,7 +177,7 @@
         </ul>
         </li>
         
-        <li class="nav__item">
+        <li class="nav__item li3">
         <h2 class="nav__title">Legal</h2>
         
         <ul class="nav__ul">
@@ -184,5 +200,10 @@
         <p>&copy; 2023 Labs. All rights reserved.</p>
     </div>
     </footer>
+
+    <!-- java script -->
+    <script src="<?php echo APPROOT.'\public\js\Home.js';?>"></script>
+
+
 </body>
 </html>
--- app/views/Login.php
+++ app/views/Login.php
@@ -5,39 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/login.css'?>">
     <title>Login Form</title>
+    <script src="<?php echo APPROOT.'\public\js\login.js';?>"></script>
 </head>
-<body>
+<body onload="select()">
     <?php require_once 'components/home.php'?>
     <div class="login-container">
-        <div class="login-box">
-            <h2>Login as <span>Employee</span></h2>
-            <div class="horizontal-line"></div>
+        <h1>SIGN IN TO YOUR ACCOUNT</h1>
+        <div class="login-button" >
+            <button onclick="employee()" id="employee-button">AS A <span>EMPLOYEE</span></button>
+            <button onclick="patient()" id="patient-button">AS A <span>PATIENT</span></button>
+        </div>
+
+        <div class="employee" id="employee">
             <form action="<?php echo URLROOT?>Employee/login?>" method="post">
                 <div class="form-group">
-                    <input type="email" id="employee-Email" name="employee-Email" placeholder="Email" value="" required><br>
+                    <label for="employee-Email">Email</label><br>
+                    <input type="email" id="employee-Email" name="employee-Email" value="" required><br>
                 </div>
                 <div class="form-group">
-                    <input type="password" id="employee-password" name="employee-password" value="" placeholder="Password"><br>
+                    <label for="employee-password">Password</label><br>
+                    <input type="password" id="employee-password" name="employee-password" value=""><br>
                     <span class="formerr"><?php echo $data['empformerr'];?></span>
+                    <a href="#">Forgot Password</a>
                 </div>
                 <button type="submit" name="submit" class="btn-login button">Login</button>
+                <p>Not yet a member?<a href="">Please contact admin</a></p>
             </form>
         </div>
-        <div class="vertical-line"></div>
-        <div class="login-box">
-            <h2>Login as <span>Patient</span></h2>
-            <div class="horizontal-line"></div>
-            <form action="<?php echo URLROOT?>User/login?>" method="post">
-                <div class="form-group patient">
-                    <input type="email" id="patient-Email" name="patient-Email" value="" placeholder="Email" value='' required><br>
-                </div>
-                <div class="form-group patient">
-                    <input type="password" id="patient-password" name="patient-password" value="" placeholder="Password" value='' required><br>
-                    <span class="formerr"><?php echo $data['formerr'];?></span>
-                </div>
-                <button type="submit" name="submit" class="btn-login button">Login</button>
-            </form>
+
+        <div class="patient" id="patient">
+                <form action="<?php echo URLROOT?>User/login?>" method="post">
+                    <div class="form-group patient">
+                        <label for="employee-Email">Email</label><br>
+                        <input type="email" id="patient-Email" name="patient-Email" value="" value='' required><br>
+                    </div>
+                    <div class="form-group patient">
+                        <label for="employee-password">Password</label><br>
+                        <input type="password" id="patient-password" name="patient-password" value="" value='' required><br>
+                        <span class="formerr"><?php echo $data['formerr'];?></span>
+                        <a href="#">Forgot Password</a>
+                    </div>
+                    <button type="submit" name="submit" class="btn-login button">Login</button>
+                    <p>Not yet a member?<a href="http://localhost/labora/user/register">SignUp Now</a></p>
+                </form>
         </div>
     </div>
+    
 </body>
 </html>

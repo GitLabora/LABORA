05068ef39dfe0b5ffbbaab0cce93b2cff8f96b54
AshenSachinthana <dasachinthana@gmail.com>
2023-12-10 20:20:38 +0530

    MLT dashboard

---
+++ app/views/mltdashboard/medicalTests.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="refresh" content="600; url=http://localhost/labora/user/logout"> -->
+    <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/mltdashboard/medicalTests.css'?>">
+    <script src="<?php echo APPROOT.'/public/js/mltdashboard/patient.js';?>"></script>
+    <!-- static icons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <!-- annimation icons -->
+    <script src="https://cdn.lordicon.com/lordicon-1.1.0.js"></script>
+    <title>MLT dashboard</title>
+</head>
+<body>
+    <?php require_once 'components/nevbar.php' ?>
+
+    <div class="container_1">
+
+        <div class="tablename">
+            <h3>Testing Details</h3>
+        </div>
+        <div class="line"></div>
+        <div class="searchbar">
+            <input type="text" class="search" placeholder="Enter test name or short name">
+            <a href="#" class="searchbtn">Search</a>
+        </div>
+
+        <div class="filter">
+            <form action="#" method="post">
+                <input type="text" class="test-type" placeholder="Enter test type">
+                <!-- <input type="date" class="from">
+                <input type="date" class="to"> -->
+                <button type="submit" class="submit button">Filter</button>
+            </form>
+        </div>
+        <div class="add">
+            
+            <a href="http://localhost/labora/PatientDashboard/test_form" class="addbtn"><ion-icon name="add"></ion-icon> Create New</a>
+         </div>
+        <div>
+        <table>
+    <thead>
+        <tr>
+            <th>Test ID</th>
+            <th>Test Name</th>
+            <th>Short Name</th>
+            <th>Test Type</th>
+            <th>Availability</th>
+            <!-- <th>Appointment Time</th>
+            <th>Appointment Duration</th>
+            <th>Appointment Status</th>
+            <th>Appointment Notes</th> -->
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Blood Test</td>
+            <td>BT</td>
+            <td>Check-up</td>
+            <!-- <td>10:00 AM</td>
+            <td>30 minutes</td>
+            <td>Scheduled</td>
+            <td>Reminder call sent</td> -->
+            <td><button class="cancel">yes</button></td>
+            <td><a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
+            <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>X-ray Examination</td>
+            <td>XRE</td>
+            <td>MRI Scan</td>
+            <!-- <td>02:30 PM</td>
+            <td>1 hour</td>
+            <td>Completed</td>
+            <td>N/A</td> -->
+            <td><button class="cancel del">no</button></td>
+            <td><a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
+            <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Electrocardiogram</td>
+            <td>ECG</td>
+            <td>Check-up</td>
+            <!-- <td>09:15 AM</td>
+            <td>45 minutes</td>
+            <td>Canceled</td>
+            <td>Patient rescheduled</td> -->
+            <td><button class="cancel del">no</button></td>
+            <td><a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
+            <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Urine Analysis</td>
+            <td>UA</td>
+            <td>Check-up</td>
+            <!-- <td>10:00 AM</td>
+            <td>30 minutes</td>
+            <td>Scheduled</td>
+            <td>Reminder call sent</td> -->
+            <td><button class="cancel">yes</button></td>
+            <td><a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
+            <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>CT Scan</td>
+            <td>CTS</td>
+            <td>MRI Scan</td>
+            <!-- <td>02:30 PM</td>
+            <td>1 hour</td>
+            <td>Completed</td>
+            <td>N/A</td> -->
+            <td><button class="cancel del">no</button></td>
+            <td><a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
+            <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Electrocardiogram</td>
+            <td>ECG</td>
+            <td>Check-up</td>
+            <!-- <td>09:15 AM</td>
+            <td>45 minutes</td>
+            <td>Canceled</td>
+            <td>Patient rescheduled</td> -->
+            <td><button class="cancel del">no</button></td>
+            <td><a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
+            <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Ultrasound Scan</td>
+            <td>USS</td>
+            <td>Check-up</td>
+            <!-- <td>10:00 AM</td>
+            <td>30 minutes</td>
+            <td>Scheduled</td>
+            <td>Reminder call sent</td> -->
+            <td><button class="cancel">yes</button></td>
+            <td><a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
+            <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a></td>
+        </tr>
+        <!-- Add more rows as needed -->
+    </tbody>
+</table>
+
+        </div>
+    </div>
+</body>
+</html>
---
+++ app/views/mltdashboard/reports.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="refresh" content="600; url=http://localhost/labora/user/logout"> -->
+    <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/mltdashboard/report2.css'?>">
+    <script src="<?php echo APPROOT.'/public/js/mltdashboard/patient.js';?>"></script>
+    <!-- static icons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <!-- annimation icons -->
+    <script src="https://cdn.lordicon.com/lordicon-1.1.0.js"></script>
+    <title>MLT dashboard</title>
+</head>
+<body>
+    <?php require_once 'components/nevbar.php' ?>
+    <div class="container_1">
+
+    <div class="tablename">
+            <h3>Medical Reports</h3>
+        </div>
+        <div class="line"></div>
+        <div class="searchbar">
+            <input type="text" class="search" placeholder="Enter reference number">
+            <a href="#" class="searchbtn">Search</a>
+        </div>
+
+        <div class="filter">
+            <form action="#" method="post">
+                <!-- <input type="text" class="test-type" placeholder="Enter test type"> -->
+                <select type="text" class="test-type">
+                <option value="option1">All</option>
+                <option value="option1">Pending</option>
+                <option value="option2">Completed</option>
+                </select>
+                <!-- <input type="date" class="from">
+                <input type="date" class="to"> -->
+                <button type="submit" class="submit button">Filter</button>
+            </form>
+        </div>
+        <!-- <div class="add">
+            
+            <a href="#" class="addbtn"><ion-icon name="add"></ion-icon> Schedule appointment</a>
+         </div> -->
+        <div>
+        <table>
+    <thead>
+        <tr>
+            <th>Index</th>
+            <th>Ref No</th>
+            <th>Test Type</th>
+            <th>Arrival Date</th>
+            <th>Arrival Time</th>
+            <th>Status</th>
+            <!-- <th>Approve Status</th> -->
+            <th>Notes</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>AP12345</td>
+            <td>Check-up</td>
+            <td>2023-11-15</td>
+            <td>10:00 AM</td>
+            <td>Pending</td>
+            <!-- <td>Scheduled</td> -->
+            <td>Reminder call sent</td>
+            <!-- <td><button class="cancel">Cancel</button></td> -->
+            <td><a href="#" class="download"><ion-icon name="arrow-forward-circle-outline"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>AP54321</td>
+            <td>MRI Scan</td>
+            <td>2023-11-20</td>
+            <td>02:30 PM</td>
+            <td>Completed</td>
+            <!-- <td>Completed</td> -->
+            <td>Valid</td>
+            <!-- <td><button class="cancel del">Delete</button></td> -->
+            <td><a href="#" class="download"><ion-icon name="arrow-forward-circle-outline"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>AP98765</td>
+            <td>Dental Check-up</td>
+            <td>2023-11-25</td>
+            <td>09:15 AM</td>
+            <td>Pending</td>
+            <!-- <td>Canceled</td> -->
+            <td>Invalid</td>
+            <!-- <td><button class="cancel del">Delete</button></td> -->
+            <td><a href="#" class="download"><ion-icon name="arrow-forward-circle-outline"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>AP12345</td>
+            <td>Check-up</td>
+            <td>2023-11-15</td>
+            <td>10:00 AM</td>
+            <td>Pending</td>
+            <!-- <td>Scheduled</td> -->
+            <td>Valid</td>
+            <!-- <td><button class="cancel">Cancel</button></td> -->
+            <td><a href="#" class="download"><ion-icon name="arrow-forward-circle-outline"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>AP54321</td>
+            <td>MRI Scan</td>
+            <td>2023-11-20</td>
+            <td>02:30 PM</td>
+            <td>Completed</td>
+            <!-- <td>Completed</td> -->
+            <td>Valid</td>
+            <!-- <td><button class="cancel del">Delete</button></td> -->
+            <td><a href="#" class="download"><ion-icon name="arrow-forward-circle-outline"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>AP98765</td>
+            <td>Dental Check-up</td>
+            <td>2023-11-25</td>
+            <td>09:15 AM</td>
+            <td>Pending</td>
+            <!-- <td>Canceled</td> -->
+            <td>Invalid</td>
+            <!-- <td><button class="cancel del">Delete</button></td> -->
+            <td><a href="#" class="download"><ion-icon name="arrow-forward-circle-outline"></ion-icon></a></td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>AP12345</td>
+            <td>Check-up</td>
+            <td>2023-11-15</td>
+            <td>10:00 AM</td>
+            <td>Pending</td>
+            <!-- <td>Scheduled</td> -->
+            <td>Valid</td>
+            <!-- <td><button class="cancel">Cancel</button></td> -->
+            <td><a href="#" class="download"><ion-icon name="arrow-forward-circle-outline"></ion-icon></ion-icon></a></td>
+        </tr>
+        <!-- Add more rows as needed -->
+    </tbody>
+        </div>
+    </div>
+</body>
+</html>

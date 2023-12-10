5435a17db80a72dc9dbc94f8ac0519ccc9bab979
AshenSachinthana <dasachinthana@gmail.com>
2023-12-10 19:43:26 +0530

    file change

--- app/bootloader.php
+++ app/bootloader.php
@@ -1,6 +1,5 @@
 <?php
     require_once 'helpers/session_helper.php';
-    require_once 'helpers/send_email_helper.php';
     require_once 'config/config.php';
     require_once 'libraries/Core.php';
     require_once 'libraries/Controller.php';

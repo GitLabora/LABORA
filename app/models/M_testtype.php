010af42db401b2936dc8d00c0fab670bd0406eab
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-01-21 03:09:42 +0530

    implement appointment

--- app/models/M_testtype.php
+++ app/models/M_testtype.php
@@ -25,10 +25,10 @@
 
 
             public function getDuration($test_type){
-                $result =mysqli_query($this->conn , "SELECT * FROM test_type WHERE Test_type='$test_type'") ;
+                $result =mysqli_query($this->conn , "SELECT * FROM test_type WHERE id='$test_type'") ;
                 if(mysqli_num_rows($result)>0){
                     $row = mysqli_fetch_assoc($result);
-                    return $row["Time_duration"];
+                    return $row;
                     
                 }  
             }
@@ -37,6 +37,14 @@
                 $result =mysqli_query($this->conn , "SELECT * FROM test_type");
                 $result_set = mysqli_fetch_all( $result  , MYSQLI_ASSOC);
                 return $result_set;       
+            }
+
+            public function getAvailableTime( $date , $start_time , $end_time){
+                $result_set = mysqli_query($this->conn, "SELECT * FROM appointment WHERE Appointment_Date = '$date' AND Appointment_Time BETWEEN '$start_time' AND '$end_time' ORDER BY Appointment_Time DESC LIMIT 1;");
+                if(mysqli_num_rows($result_set)){
+                    $row = mysqli_fetch_assoc($result_set);
+                    return $row;
+                }
             }
     }
 ?>
